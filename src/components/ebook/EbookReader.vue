74685b74515e4667f0a508123996e7caa79daaa9
jinjun1994 <jinjun199403@163.com>
2018-12-31 07:29:18 +0800

    touch to change page

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -15,11 +15,46 @@
       ...mapGetters(['fileName'])
     },
     methods: {
+      prevPage() {
+        if (this.rendition) {
+          this.rendition.prev()
+        }
+      },
+      nextPage() {
+        if (this.rendition) {
+          this.rendition.next()
+        }
+      },
+      toggleTitleAndMenu() {},
       initEpub() {
         const url = 'http://127.0.0.1/book/' + this.fileName + '.epub'
         this.book = new Epub(url)
         console.log(this.book)
         console.log(url)
+        this.rendition = this.book.renderTo('read', {
+          width: innerWidth,
+          height: innerHeight,
+          method: 'default'
+          // 微信兼容
+        })
+        this.rendition.display()
+        this.rendition.on('touchstart', event => {
+          this.touchStartX = event.changedTouches[0].clientX
+          this.touchStartTime = event.timeStamp
+        })
+        this.rendition.on('touchend', event => {
+          const offSet = event.changedTouches[0].clientX - this.touchStartX
+          const touchTime = event.timeStamp - this.touchStartTime
+          if (touchTime < 500 && offSet > 40) {
+             this.prevPage()
+          } else if ((touchTime < 500 && offSet < -40)) {
+            this.nextPage()
+          } else {
+            this.toggleTitleAndMenu()
+          }
+          event.preventDefault()
+          event.stopPropagation()
+        })
       }
     },
     mounted() {
